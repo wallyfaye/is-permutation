37875e64059933c245e83ede8dca343b521fc6e6
Wally Faye <wally.faye@gmail.com>
2017-06-19 21:51:52 -0400

    simple solution

--- IsPermutation.java
+++ IsPermutation.java
@@ -2,8 +2,45 @@ class IsPermutation{
 
 	public boolean res = false;
 	public IsPermutation(String[] args){
-		System.out.println(args[0]);
-		System.out.println(args[1]);
+
+		String word_1 = args[0];
+		String word_2 = args[1];
+
+		if(word_1.length() != word_2.length()){
+			return;
+		}
+
+		int[] char_array = new int[500];
+		boolean is_perm = true;
+
+		for(int i = 0; i < word_1.length(); i++){
+
+			char this_char = word_1.charAt(i);
+			int char_num = Character.getNumericValue(this_char);
+
+			char_array[char_num]++;
+
+		}
+
+		for(int i = 0; i < word_2.length(); i++){
+
+			char this_char = word_2.charAt(i);
+			int char_num = Character.getNumericValue(this_char);
+
+			char_array[char_num]--;
+
+			if(char_array[char_num] < 0){
+				is_perm = false;
+			}
+
+		}
+
+		if(is_perm){
+
+			res = true;
+
+		}
+
 	}
 
 }
